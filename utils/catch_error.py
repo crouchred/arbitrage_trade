6a218462780d72d437205b0dc0b89d08f756cefc
chenxuyuan <crouchred@gmail.com>
2018-06-19 10:55:52 +0800

    不发送KeyboardInterrupt的错误

--- utils/catch_error.py
+++ utils/catch_error.py
@@ -3,7 +3,7 @@ import traceback
 from utils.logger import Logger
 logger = Logger.get_logger(__file__)
 
-client = Client('https://459d646b66124477bce25177fa98423e:4f9edae9fbd140da82bea7f99ffd3318@sentry.io/1222147')
+client = Client('https://459d646b66124477bce25177fa98423e:4f9edae9fbd140da82bea7f99ffd3318@sentry.io/1222147', IGNORE_EXCEPTIONS=['KeyboardInterrupt'])
 
 def catch_decorator(func):
     def __wrapper(*args, **kwargs):
